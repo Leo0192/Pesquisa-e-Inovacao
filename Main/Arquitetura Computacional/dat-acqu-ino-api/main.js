f157ad282a1385fdf10e7355d74a27e13508f39a
Leo0192 <leonardo.tubero@sptech.school>
2025-04-22 14:05:00 -0300

    Mudança na porta da API do arduino

--- Main/Arquitetura Computacional/dat-acqu-ino-api/main.js
+++ Main/Arquitetura Computacional/dat-acqu-ino-api/main.js
@@ -37,7 +37,7 @@ const serial = async (
 
     // lista as portas seriais disponíveis e procura pelo Arduino
     const portas = await serialport.SerialPort.list();
-    const portaArduino = portas.find((porta) => porta.vendorId == '1A86' && porta.productId == 7523); // 2341:43 é o ID do Arduino Uno original sptech, o genérico é '1A86':7523
+    const portaArduino = portas.find((porta) => porta.vendorId == '2341' && porta.productId == 43); // 2341:43 é o ID do Arduino Uno original sptech, o genérico é '1A86':7523
     if (!portaArduino) {
         throw new Error('O arduino não foi encontrado em nenhuma porta serial');
     }
