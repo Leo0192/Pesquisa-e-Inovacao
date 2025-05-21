f28120768c0d1cf0e69ba6ab9a5a6463bd676106
guilhermesdias45 <guilherme.sdias@sptech.school>
2025-05-21 12:41:38 -0300

    Adaptações em medida MVC para se adequar ao nosso projeto

--- Main/Website/src/models/medidaModel.js
+++ Main/Website/src/models/medidaModel.js
@@ -1,28 +1,26 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idTalhao, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
+                        umidade,
+                        data,
+                        DATE_FORMAT(data,'%H:%i:%s') as momento_grafico
+                    FROM historico_sensor
+                    WHERE fkSensor = ${idTalhao}
                     ORDER BY id DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idTalhao) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
+                        umidade,
+                        DATE_FORMAT(data,'%H:%i:%s') as momento_grafico, 
+                        fkSensor 
+                        FROM historico_sensor WHERE fkSensor = ${idTalhao} 
                     ORDER BY id DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
