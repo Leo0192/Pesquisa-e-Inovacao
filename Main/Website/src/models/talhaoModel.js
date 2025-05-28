3054c406ef36072ff7b6580cd2458d2e45c87d7b
HerculesSP <hercules.pereira@sptech.school>
2025-05-28 11:50:36 -0300

    Teste da página de login

---
+++ Main/Website/src/models/talhaoModel.js
@@ -0,0 +1,14 @@
+var database = require("../database/config");
+
+function buscarTalhoesPorEmpresa(empresaId) {
+
+  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+module.exports = {
+  buscarTalhoesPorEmpresa,
+  
+}
